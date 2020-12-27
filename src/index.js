a1d9ae7f92e53fc727108afd49489eb2eb00ca91
luissagit <casualulie@gmail.com>
2020-12-27 16:23:05 +0700

    adding react-snapshot

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { render } from 'react-snapshot';
 
-render(
+ReactDOM.render(
     <App />,
   document.getElementById('root')
 );
